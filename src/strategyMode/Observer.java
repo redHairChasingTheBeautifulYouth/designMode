d4f13710754bdda5a69a26f7492bd495b5d2256c
liubing <576192031@qq.com>
2018-04-16 23:26:00 +0800

    增加观察者模式

---
+++ src/strategyMode/Observer.java
@@ -0,0 +1,10 @@
+package strategyMode;/*
+ *一句话描述该类作用:
+ *@Author:
+ *LB
+ *
+ */
+
+public interface Observer {
+    public abstract void update();
+}
---
+++ src/strategyMode/Subject.java
@@ -0,0 +1,12 @@
+package strategyMode;/*
+ *一句话描述该类作用:
+ *@Author:
+ *LB
+ *
+ */
+
+import java.util.List;
+
+public abstract class Subject {
+    private List<>
+}
